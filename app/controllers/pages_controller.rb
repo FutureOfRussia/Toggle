40f6726de986c1e3a44c821d55f611c002a58bb4
Nikita Zalygin <futureofrussia@list.ru>
2017-02-17 14:18:52 +0300

    add cookies

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,13 @@ class PagesController < ApplicationController
 	def home
 		if log_in?
 
-			if session[:token].present? 
-				vk = VkontakteApi::Client.new(session[:token])
-			else
-				session[:token] = cookies[:token]
-				vk = VkontakteApi::Client.new(session[:token])
-			end
+			session[:token] = cookies[:token] if !session[:token].present?
+				
+			vk = VkontakteApi::Client.new(session[:token])
 
-		@user = vk.users.get(uid: session[:vk_id], fields: [:screen_name, :photo]).first
-		@friends = vk.friends.get
-		@photos = vk.photos.get(owner_id: 78141952, album_id: 'saved')
+			@user = vk.users.get(uid: session[:vk_id], fields: [:screen_name, :photo]).first
+			@friends = vk.friends.get
+			@photos = vk.photos.get(owner_id: 78141952, album_id: 'saved')
 
 		end
 	end
