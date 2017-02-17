945f6b82b4629f949f5145c22a9f2a74dab2c9e1
Nikita Zalygin <futureofrussia@list.ru>
2017-02-17 15:19:18 +0300

    update friends list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
 			vk = VkontakteApi::Client.new(session[:token])
 
 			@user = vk.users.get(uid: session[:vk_id], fields: [:screen_name, :photo]).first
-			@friends = vk.friends.get
+			@friends = vk.friends.get(order: 'hints', fields: [:screen_name, :name, :photo])
 			@photos = vk.photos.get(owner_id: 78141952, album_id: 'saved')
 
 		end
