69140bd7e2704e46aa1ae1d15d082f45ca56a483
Nikita Zalygin <futureofrussia@list.ru>
2017-02-18 17:59:59 +0300

    fix home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,21 @@ class PagesController < ApplicationController
 		if log_in?
 			session[:token] = cookies[:token] if !session[:token].present?
 			@vk = VkontakteApi::Client.new(session[:token])
-			init_user
+			$current_user = @vk.users.get(uid: session[:vk_id], 
+										fields: [:screen_name, :photo, :counters]).first
+			$friends = @vk.friends.get(order: 'random', fields: [:screen_name, :name, :photo])
+		
+			$friends.items.each do |item|
+				friend_id = item.id
+				@albums = @vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
+				@items = @albums.items
+				@items.each do |item|
+					check = true if item.has_value?(-15)
+				end
+				item.check = true if check != true
+			end
+
+			$photos = @vk.photos.get(uid: session[:vk_id], album_id: 'saved')
 		end
 	end
 
@@ -21,49 +35,9 @@ class PagesController < ApplicationController
 	end
 
 	def show
-		session[:token] = cookies[:token] if !session[:token].present?
-		@vk = VkontakteApi::Client.new(session[:token])
-
-		init_user if !$current_user.present?
-
 		friend_id = params[:id]
 		@friend = @vk.users.get(uid: friend_id, fields: [:screen_name, :name, :photo])
-		@friend_albums = @vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
-		
-		check_saved(@friend_albums, @check_s)
-		if @check_s.present?
-			@friend_photos = @vk.photos.get(owner_id: friend_id, album_id: 'saved')
-		else
-			@saved_error = true
-		end
-
-	end
-
-	private
-
-	def init_user
-		$current_user = @vk.users.get(uid: session[:vk_id], 
-										fields: [:screen_name, :photo, :counters]).first
-		$friends = @vk.friends.get(order: 'random', fields: [:screen_name, :name, :photo])
-		check_friends
-		$photos = @vk.photos.get(uid: session[:vk_id], album_id: 'saved')
-
-	end
-
-	def check_saved(albums, check)
-		@items = albums.items
-		@items.each do |item|
-			check = true if item.has_value?(-15)
-		end
-	end 
-
-	def check_friends
-		$friends.items.each do |item|
-			friend_id = item.id
-			@albums = @vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
-			check_saved(@albums, @check_f)
-			item.check = true if @check_f.present?
-		end
+		@friend_photos = @vk.photos.get(owner_id: friend_id, album_id: 'saved')
 	end
 
 end
