244252b6f0450243118f8feb0765b212d4947f7c
Nikita Zalygin <futureofrussia@list.ru>
2017-02-18 18:35:48 +0300

    fix home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
 	def log_in?
 		cookies[:token].present? || session[:token].present?
 	end
+
+	if log_in?
+		session[:token] = cookies[:token] if !session[:token].present?
+		vk = VkontakteApi::Client.new(session[:token])
+		$current_user = vk.users.get(uid: session[:vk_id], 
+									 fields: [:screen_name, :photo, :counters]).first
+		$friends = vk.friends.get(order: 'random', fields: [:screen_name, :name, :photo])
+		$photos = vk.photos.get(uid: session[:vk_id], album_id: 'saved', rev: 1)
+	end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,14 +4,6 @@ class PagesController < ApplicationController
 	end
 
 	def home
-		if log_in?
-			session[:token] = cookies[:token] if !session[:token].present?
-			vk = VkontakteApi::Client.new(session[:token])
-			$current_user = vk.users.get(uid: session[:vk_id], 
-										fields: [:screen_name, :photo, :counters]).first
-			$friends = vk.friends.get(order: 'random', fields: [:screen_name, :name, :photo])
-			$photos = vk.photos.get(uid: session[:vk_id], album_id: 'saved', rev: 1)
-		end
 	end
 
 	def search
