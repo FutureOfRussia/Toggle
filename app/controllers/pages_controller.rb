15ee78b1d3b982979ac333c7cd63ea47adbeb386
Nikita Zalygin <futureofrussia@list.ru>
2017-02-19 00:44:37 +0300

    fix home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,10 +7,10 @@ class PagesController < ApplicationController
 		if log_in?
 			session[:token] = cookies[:token] if !session[:token].present?
 			vk = VkontakteApi::Client.new(session[:token])
-			$current_user = vk.users.get(uid: session[:vk_id], 
+			@current_user = vk.users.get(uid: session[:vk_id], 
 										 fields: [:screen_name, :photo, :counters]).first
-			$friends = vk.friends.get(order: 'random', fields: [:screen_name, :name, :photo])
-			$photos = vk.photos.get(uid: session[:vk_id], album_id: 'saved', rev: 1)
+			@friends = vk.friends.get(order: 'random', fields: [:screen_name, :name, :photo])
+			@photos = vk.photos.get(uid: session[:vk_id], album_id: 'saved', rev: 1)
 		end
 	end
 
@@ -18,7 +18,7 @@ class PagesController < ApplicationController
 		if log_in?
 			search = params[:search]
 			vk = VkontakteApi::Client.new(session[:token])
-			$search_friends = vk.friends.search(q: search, fields: [:screen_name, :name, :photo])
+			@search_friends = vk.friends.search(q: search, fields: [:screen_name, :name, :photo])
 			respond_to do |format|
 				format.js
 			end	
@@ -33,14 +33,14 @@ class PagesController < ApplicationController
 			session[:token] = cookies[:token] if !session[:token].present?
 			vk = VkontakteApi::Client.new(session[:token])
 			friend_id = params[:id]
-			$friend = vk.users.get(uid: friend_id, fields: [:screen_name, :name, :photo])
-			@albums = vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
-			items = @albums.items
+			@friend = vk.users.get(uid: friend_id, fields: [:screen_name, :name, :photo]).first
+			albums = vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
+			items = albums.items
 				items.each do |item|
 					@check = 1 if item.has_value?(-15)
 				end
 			if @check.present?
-				$friend_photos = vk.photos.get(owner_id: friend_id, album_id: 'saved', rev: 1)
+				@friend_photos = vk.photos.get(owner_id: friend_id, album_id: 'saved', rev: 1)
 			else
 				redirect_to root_url
 				flash[:danger] = "Пользователь ограничил доступ к фотографиям."
