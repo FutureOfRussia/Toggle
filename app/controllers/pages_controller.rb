9748219eb4be1bc6a67aef5f4ea0d7f9a6d62024
Nikita Zalygin <futureofrussia@list.ru>
2017-02-18 16:51:16 +0300

    fix home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,15 +6,15 @@ class PagesController < ApplicationController
 	def home
 		if log_in?
 			session[:token] = cookies[:token] if !session[:token].present?
-			vk = VkontakteApi::Client.new(session[:token])
+			@vk = VkontakteApi::Client.new(session[:token])
 			init_user
 		end
 	end
 
 	def search
 		search = params[:search]
-		vk = VkontakteApi::Client.new(session[:token])
-		$search_friends = vk.friends.search(q: search, fields: [:screen_name, :name, :photo])
+		@vk = VkontakteApi::Client.new(session[:token])
+		$search_friends = @vk.friends.search(q: search, fields: [:screen_name, :name, :photo])
 		respond_to do |format|
 			format.js
 		end	
@@ -22,17 +22,17 @@ class PagesController < ApplicationController
 
 	def show
 		session[:token] = cookies[:token] if !session[:token].present?
-		vk = VkontakteApi::Client.new(session[:token])
+		@vk = VkontakteApi::Client.new(session[:token])
 
 		init_user if !$current_user.present?
 
 		friend_id = params[:id]
-		@friend = vk.users.get(uid: friend_id, fields: [:screen_name, :name, :photo])
-		@friend_albums = vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
+		@friend = @vk.users.get(uid: friend_id, fields: [:screen_name, :name, :photo])
+		@friend_albums = @vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
 		
 		check_saved
 		if check.present?
-			@friend_photos = vk.photos.get(owner_id: friend_id, album_id: 'saved')
+			@friend_photos = @vk.photos.get(owner_id: friend_id, album_id: 'saved')
 		else
 			@saved_error = 1
 		end
@@ -49,9 +49,9 @@ class PagesController < ApplicationController
 	end 
 
 	def init_user
-		$current_user = vk.users.get(uid: session[:vk_id], 
+		$current_user = @vk.users.get(uid: session[:vk_id], 
 										fields: [:screen_name, :photo, :counters]).first
-		$friends = vk.friends.get(order: 'random', fields: [:screen_name, :name, :photo])
-		$photos = vk.photos.get(uid: session[:vk_id], album_id: 'saved')
+		$friends = @vk.friends.get(order: 'random', fields: [:screen_name, :name, :photo])
+		$photos = @vk.photos.get(uid: session[:vk_id], album_id: 'saved')
 	end
 end
