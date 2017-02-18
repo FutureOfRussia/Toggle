6677c8a9f9d88ec79968b63dd8db56328fafbb2f
Nikita Zalygin <futureofrussia@list.ru>
2017-02-18 17:41:40 +0300

    fix home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -30,28 +30,40 @@ class PagesController < ApplicationController
 		@friend = @vk.users.get(uid: friend_id, fields: [:screen_name, :name, :photo])
 		@friend_albums = @vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
 		
-		check_saved
-		if @check.present?
+		check_saved(@friend_albums, @check_s)
+		if @check_s.present?
 			@friend_photos = @vk.photos.get(owner_id: friend_id, album_id: 'saved')
 		else
-			@saved_error = 1
+			@saved_error = true
 		end
 
 	end
 
 	private
 
-	def check_saved
-		@items = @friend_albums.items
-		@items.each do |item|
-			@check = 1 if item.has_value?(-15)
-		end
-	end 
-
 	def init_user
 		$current_user = @vk.users.get(uid: session[:vk_id], 
 										fields: [:screen_name, :photo, :counters]).first
 		$friends = @vk.friends.get(order: 'random', fields: [:screen_name, :name, :photo])
+		check_friends
 		$photos = @vk.photos.get(uid: session[:vk_id], album_id: 'saved')
+
 	end
+
+	def check_saved(albums, check)
+		@items = albums.items
+		@items.each do |item|
+			check = true if item.has_value?(-15)
+		end
+	end 
+
+	def check_friends
+		$friends.items.each do |item|
+			friend_id = item.id
+			@albums = @vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
+			check_saved(@albums, @check_f)
+			item.check = true if @check_f.present?
+		end
+	end
+
 end
