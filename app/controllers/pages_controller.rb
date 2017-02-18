9b96c5d16732cc33a0f9e19cc2404e4d3a484886
Nikita Zalygin <futureofrussia@list.ru>
2017-02-18 18:21:43 +0300

    fix home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,17 +10,6 @@ class PagesController < ApplicationController
 			$current_user = @vk.users.get(uid: session[:vk_id], 
 										fields: [:screen_name, :photo, :counters]).first
 			$friends = @vk.friends.get(order: 'random', fields: [:screen_name, :name, :photo])
-		
-			$friends.items.each do |item|
-				friend_id = item.id
-				@albums = @vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
-				@items = @albums.items
-				@items.each do |item|
-					@ch = true if item.has_value?(-15)
-				end
-				item[:check] = true if @ch != true
-			end
-
 			$photos = @vk.photos.get(uid: session[:vk_id], album_id: 'saved')
 		end
 	end
@@ -38,6 +27,11 @@ class PagesController < ApplicationController
 		friend_id = params[:id]
 		@friend = @vk.users.get(uid: friend_id, fields: [:screen_name, :name, :photo])
 		@friend_photos = @vk.photos.get(owner_id: friend_id, album_id: 'saved')
+		@albums = @vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
+		items = @albums.items
+			items.each do |item|
+				@check = true if item.has_value?(-15)
+			end
 	end
 
 end
