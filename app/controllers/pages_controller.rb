3fe94ed4ada74480ddfaa75e20be2a769cc55d15
Nikita Zalygin <futureofrussia@list.ru>
2017-02-19 01:03:05 +0300

    fix home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
 		if log_in?
 			session[:token] = cookies[:token] if !session[:token].present?
 			vk = VkontakteApi::Client.new(session[:token])
-			@current_user = vk.users.get(uid: session[:vk_id], 
+			@current_user = vk.users.get(user_ids: session[:vk_id], 
 										 fields: [:screen_name, :photo, :counters]).first
 			@friends = vk.friends.get(order: 'random', fields: [:screen_name, :name, :photo])
 			@photos = vk.photos.get(uid: session[:vk_id], album_id: 'saved', rev: 1)
@@ -33,7 +33,7 @@ class PagesController < ApplicationController
 			session[:token] = cookies[:token] if !session[:token].present?
 			vk = VkontakteApi::Client.new(session[:token])
 			friend_id = params[:id]
-			@friend = vk.users.get(uid: friend_id, fields: [:screen_name, :name, :photo])
+			@friend = vk.users.get(user_ids: friend_id, fields: [:screen_name, :name, :photo])
 			albums = vk.photos.getAlbums(owner_id: friend_id, need_system: 1)
 			items = albums.items
 				items.each do |item|
