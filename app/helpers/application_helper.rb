7d77c84c5209c6424ebbfaad5c81a40ba53aa3e1
Nikita Zalygin <futureofrussia@list.ru>
2017-02-19 01:09:21 +0300

    fix home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
-def full_title(page_title)
+	def full_title(page_title)
 		base_title = "Toggle"
 		if page_title.empty?
 			base_title
 		else
 			"#{base_title} | #{page_title}"
 		end
-end
+	end
+	
+	def vk_url(user)
+		"https://vk.com/#{user.screen_name}"
+	end
+
+	def full_name(user)
+		"#{user.first_name} #{user.last_name}"
+	end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,9 +1,2 @@
 module PagesHelper
-	def vk_url(user)
-		"https://vk.com/#{user.screen_name}"
-	end
-
-	def full_name(user)
-		"#{user.first_name} #{user.last_name}"
-	end
 end
