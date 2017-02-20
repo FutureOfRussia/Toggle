6bc87ab8a4a5704fa68b7ab0967b0e64b9f0204f
Nikita Zalygin <futureofrussia@list.ru>
2017-02-21 00:13:17 +0300

    refactoring html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-	def full_title(page_title)
-		base_title = "Toggle"
-		if page_title.empty?
-			base_title
-		else
-			"#{base_title} | #{page_title}"
-		end
-	end
-	
 	def vk_url(user)
 		"https://vk.com/#{user.screen_name}"
 	end
